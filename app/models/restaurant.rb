20dd4b122a5c219fc2295592cf33d4dbcd064b62
Alex Clark <alexplclark@gmail.com>
2021-05-27 10:54:17 +0100

    rails yelp mvp

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,7 @@
 class Restaurant < ApplicationRecord
-  validates :name, :address, presence: true
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
   validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
   has_many :reviews, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,9 @@ ActiveRecord::Schema.define(version: 2021_05_26_105208) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.string "category"
+    t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
